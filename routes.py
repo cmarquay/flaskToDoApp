1fee276354c967cdaa4c474f49b50e0ea8d1c9fd
Christian MARQUAY <christianmarquay@hotmail.com>
2020-06-26 18:41:16 +0200

    Added edit route

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from flask import render_template, redirect, url_for, flash, get_flashed_messages
+from flask import render_template, redirect, url_for, flash
 from models import Task
 from datetime import datetime
 
@@ -23,3 +23,19 @@ def add():
         flash("Task added to the database")
         return redirect(url_for("index"))
     return render_template("add.html", form=form)
+
+
+@app.route("/edit/<int:task_id>", methods=["GET", "POST"])
+def edit(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        form = forms.AddTaskForm()
+        if form.validate_on_submit():
+            task.title = form.title.data
+            task.date = datetime.utcnow()
+            db.session.commit()
+            flash("Task has been updated")
+            return redirect(url_for("index"))
+        form.title.data = task.title
+        return render_template("edit.html", form=form, task_id=task_id)
+    return redirect(url_for("index"))
